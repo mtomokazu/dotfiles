8938a176049319f540d39542ec4fe5d298404309
Matsushima Tomokazu <mtomokazu@gmail.com>
2017-04-14 11:32:31 +0900

    Add .emacs.d/ and delete .emacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,46 @@
+;; global variables
+(setq
+ inhibit-startup-screen t
+ create-lockfiles nil
+ make-backup-files nil
+ column-number-mode t
+ scroll-error-top-bottom t
+ show-paren-delay 0.5
+ use-package-always-ensure t
+ sentence-end-double-space nil)
+
+;; buffer local variables
+(setq-default
+ indent-tabs-mode nil
+ tab-width 4
+ c-basic-offset 4)
+
+;; modes
+(electric-indent-mode 0)
+
+;; global keybindings
+(global-unset-key (kbd "C-z"))
+
+;; the package manager
+(require 'package)
+(setq
+ package-archives '(("gnu" . "http://elpa.gnu.org/packages/")
+		    ("org" . "http://orgmode.org/elpa/")
+		    ;("melpa" . "http://melpa.org/packages/")
+		    ("melpa" . "http://melpa.milkbox.net/packages/")
+		    ("melpa-stable" . "http://stable.melpa.org/packages/")
+		    ("mermalade" . "http://mermalade-repo.org/packages/"))
+ package-archive-priorities '(("melpa-stable" . 1)))
+
+(package-initialize)
+(when (not package-archive-contents)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+
+;;
+;; Scala (ENSIME)
+;;
+(use-package scala-mode
+	     :interpreter
+	     ("scala" . scala-mode))
